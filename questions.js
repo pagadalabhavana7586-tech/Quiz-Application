ecf42309856b9c73331ba70fd434a06eee10369b
Pagadala Bhavana <pagadalabhavana7586@gmail.com>
2026-07-03 21:24:55 +0530

    Add quiz questions and answers to questions.js

---
+++ questions.js
@@ -0,0 +1,102 @@
+const questions = [
+  {
+    question: "1. What does HTML stand for?",
+    options: [
+      "Hyper Text Markup Language",
+      "High Text Machine Language",
+      "Hyper Transfer Markup Language",
+      "Home Tool Markup Language"
+    ],
+    answer: 0
+  },
+  {
+    question: "2. Which language is used for styling web pages?",
+    options: [
+      "HTML",
+      "Python",
+      "CSS",
+      "Java"
+    ],
+    answer: 2
+  },
+  {
+    question: "3. Which language is used to make web pages interactive?",
+    options: [
+      "C",
+      "JavaScript",
+      "Java",
+      "SQL"
+    ],
+    answer: 1
+  },
+  {
+    question: "4. Which tag is used to create a hyperlink in HTML?",
+    options: [
+      "<link>",
+      "<a>",
+      "<href>",
+      "<url>"
+    ],
+    answer: 1
+  },
+  {
+    question: "5. Which company developed Java?",
+    options: [
+      "Microsoft",
+      "Google",
+      "Sun Microsystems",
+      "Apple"
+    ],
+    answer: 2
+  },
+  {
+    question: "6. What is the capital of India?",
+    options: [
+      "Mumbai",
+      "New Delhi",
+      "Hyderabad",
+      "Chennai"
+    ],
+    answer: 1
+  },
+  {
+    question: "7. Which symbol is used for comments in JavaScript?",
+    options: [
+      "<!-- -->",
+      "//",
+      "**",
+      "##"
+    ],
+    answer: 1
+  },
+  {
+    question: "8. Which data structure works on FIFO?",
+    options: [
+      "Stack",
+      "Queue",
+      "Tree",
+      "Graph"
+    ],
+    answer: 1
+  },
+  {
+    question: "9. Which keyword is used to declare a constant in JavaScript?",
+    options: [
+      "let",
+      "var",
+      "const",
+      "static"
+    ],
+    answer: 2
+  },
+  {
+    question: "10. Which protocol is used to browse websites?",
+    options: [
+      "FTP",
+      "HTTP",
+      "SMTP",
+      "TCP"
+    ],
+    answer: 1
+  }
+];
